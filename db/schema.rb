13b6f3d60ebc4f9d0150b2f21c6a2d9591782c92
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2022-02-12 12:58:09 -0600

    Uses Argon2 for password encryption

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_02_07_164028) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "password_salt", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: 6
     t.datetime "remember_created_at", precision: 6
